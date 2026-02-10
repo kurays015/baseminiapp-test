640ab77dee008ddc0e60ed95d0411ed8cde3402b
Kurays <christnarvarte15@gmail.com>
2026-02-10 16:30:18 +0800

    fixed

--- minikit.config.ts
+++ minikit.config.ts
@@ -1,8 +1,7 @@
 const ROOT_URL =
-  process.env.NEXT_PUBLIC_URL ||
-  (process.env.VERCEL_PROJECT_PRODUCTION_URL
-    ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
-    : "http://localhost:3000");
+  process.env.NODE_ENV === "production"
+    ? process.env.VERCEL_PROJECT_PRODUCTION_URL
+    : "http://localhost:3000";
 
 /**
  * MiniApp configuration object. Must follow the Farcaster MiniApp specification.
@@ -24,17 +23,17 @@ export const minikitConfig = {
     description:
       "Adsorb the power of web3 with Brushie's, a drawing art mini-app on Base. Create and share your art, and mint it as NFTs to showcase your creativity on the blockchain. Join the waitlist now to unleash your artistic potential with Brushie's!",
     screenshotUrls: [`${ROOT_URL}/brushies-portrait.png`],
-    iconUrl: `${ROOT_URL}/brushies.png`,
-    splashImageUrl: `${ROOT_URL}/brushies.png`,
+    iconUrl: `${ROOT_URL}/brushies.jpg`,
+    splashImageUrl: `${ROOT_URL}/brushies.jpg`,
     splashBackgroundColor: "#000000",
     homeUrl: ROOT_URL,
     webhookUrl: `${ROOT_URL}/api/webhook`,
     primaryCategory: "social",
     tags: ["marketing", "ads", "quickstart", "waitlist"],
-    heroImageUrl: `${ROOT_URL}/brushies.png`,
+    heroImageUrl: `${ROOT_URL}/brushies.jpg`,
     tagline: "",
     ogTitle: "",
     ogDescription: "",
-    ogImageUrl: `${ROOT_URL}/brushies.png`,
+    ogImageUrl: `${ROOT_URL}/brushies.jpg`,
   },
 } as const;
